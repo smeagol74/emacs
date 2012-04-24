fcd58de34f8c9583524ed8bed0c2ab35a8de75de
Konstantin Borisov <konstantin.borisov@hitsoft-it.com>
2012-04-24 15:57:32 +0700

    added color theme

---
+++ init.el
@@ -0,0 +1,26 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+
+;; Add in your own as you wish:
+(defvar my-packages '(color-theme
+                      iimage
+                      starter-kit
+                      starter-kit-lisp
+                      starter-kit-bindings
+                      starter-kit-eshell)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+(require 'color-theme)
+(color-theme-standard)
+(load "color-theme-manoj")
+(color-theme-manoj-dark)
