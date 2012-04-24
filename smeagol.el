38b76f34e4c7f63f06be593249a4cac13a8f4043
Konstantin Borisov <konstantin.borisov@hitsoft-it.com>
2012-04-24 17:14:17 +0700

    добавил горячих клавиш для работы с org-mode

--- smeagol.el
+++ smeagol.el
@@ -1,1 +1,7 @@
 (global-set-key (kbd "M-9") 'magit-status)
+
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
